52999d37665b566383472455c8dde73b4dda5ad5
Kamron Soldozy <2018ksoldozy@tjhsst.edu>
2016-06-03 13:37:12 -0400

    Update images.js

--- scripts/images.js
+++ scripts/images.js
@@ -1,5 +1,5 @@
 "use strict";
-var links = ["Allianz Arena.jpg", "Angela Merkel.jpg", "Autobahn.jpg", "Berlin Wall.jpg", "Black Forest Cake.jpg", "Brandenburg Gate.jpg", "Englischer Garten.jpg", "German Shepherd.jpg", "Lederhosen.jpg", "Marienplatz.jpg", "Milka Chocolate.jpg", "Neuschwanstein Castle.jpg", "Nymphenberg Palace Hall of Mirrors.jpg", "Reichstag Building.jpg", "FC Bayern.jpg", "Wurst.jpg", "Mr. Williams.jpg"];
+var links = ["Allianz Arena.jpg", "Angela Merkel.jpg", "Autobahn.jpg", "Berlin Wall.jpg", "Black Forest Cake.jpg", "Brandenburg Gate.jpg", "German Shepherd.jpg", "Lederhosen.jpg", "Marienplatz.jpg", "Milka Chocolate.jpg", "Nymphenberg Palace Hall of Mirrors.jpg", "Reichstag Building.jpg","Wurst.jpg", "Mr. Williams.jpg"];
 
 function getRandomImage() {
     if (links.length == 0) {
